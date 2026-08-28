c76d27cee7fb57f24392cbb73fae79e586518cc4
Akash Sharma <skysharma777@gmail.com>
2026-08-28 19:23:57 +0530

    added "create" and "read" notes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,36 @@
 import express from "express";
+import { Notes } from "./models/notes.model.js"
 
 const app = express()
 
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("Hello")
+
+
+app.post("/create", async (req,res) => {
+    const data = req.body
+
+    await Notes.create({
+        title: data.title,
+        description: data.description
+    })
+
+    res.status(201).json({
+        message: "Note created successfully"
+    })
 })
+
+app.get("/", async (req, res) => {
+
+    const notes = await Notes.find()
+    // res.send("Hello")
+    res.status(200).json({
+        message: "Notes fetched successfully",
+        notes,
+    })
+})
+
+
 
 
 export { app }
