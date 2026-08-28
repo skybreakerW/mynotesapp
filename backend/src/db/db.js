552df34f1ac86d92c86efb0c5ff10602e8762fb8
Akash Sharma <skysharma777@gmail.com>
2026-08-28 18:51:53 +0530

    Bug fix

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,11 +1,12 @@
 import mongoose, { mongo } from "mongoose";
+import {DB_NAME} from "../constants.js"
 
 const connectDB = async() => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.DB_URI}/${DB_NAME}`)
         console.log(`Connected to DB!`)
     } catch (error) {
-        console.log("Connection failed to DB!!")
+        console.log("Connection failed to DB!!", error)
     }
 }
 
