c3815d024d6c80c489cb8267d9f913fa63e3e9af
Akash Sharma <skysharma777@gmail.com>
2026-08-30 21:44:06 +0530

    created navigation component for UI

---
+++ frontend/src/components/Navi.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { House } from 'lucide-react';
+import { NotebookPen } from 'lucide-react';
+import { NotepadText } from 'lucide-react';
+
+const Navi = () => {
+
+    const navigate = useNavigate()
+
+  return (
+    <section className='bg-[#22242a] w-full h-[6vh] fixed bottom-0 flex justify-around items-center'>
+
+        <div className='text-white flex flex-col items-center'>
+            <button onClick={() => navigate("/")}><House color="#ffffff" /></button>
+            <p className='text-xs p-1 font-medium'>Home</p>
+        </div>
+
+        <div className='text-white flex flex-col items-center'>
+            <button onClick={() => navigate("/create")}><NotebookPen color="#ffffff" /></button>
+            <p className='text-xs p-1 font-medium'>Write</p>
+        </div>
+
+        <div className='text-white flex flex-col items-center'>
+            <button onClick={() => navigate("/notes")}><NotepadText color="#ffffff" /></button>
+            <p className='text-xs p-1 font-medium'>Notes</p>
+        </div>
+
+        
+        
+
+    </section>
+  )
+}
+
+export default Navi
