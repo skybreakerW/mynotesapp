8996e5d8d9607d34dd585ebab7540be509cf6917
Akash Sharma <skysharma777@gmail.com>
2026-08-30 22:12:39 +0530

    added navigation and resized elements

--- frontend/src/pages/Notes.jsx
+++ frontend/src/pages/Notes.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import axios from "axios"
 import Addbtn from '../components/Addbtn.jsx';
+import Navi from "../components/Navi.jsx"
 
 const Notes = () => {
     const [notes, setNotes] = useState([])
@@ -18,23 +19,25 @@ const Notes = () => {
 
   return (
     <>
-    
-    <section className='min-h-screen bg-[#22242a] flex flex-col flex-wrap justify-center items-center gap-4'>
+        
+        <div className='min-h-screen relative'><Navi/>
+        <section className=' bg-[#22242a] flex flex-col flex-wrap justify-center items-center gap-4'>
 
-    {notes.length > 0 ? (
-        notes.map((note)=> (
-        <div key={note._id} className='h-auto w-90 bg-slate-500 text-white rounded-2xl p-2'>
-            <h1 className='text-2xl p-1 font-bold tracking-wider'>{note.title}</h1>
-            <h3 className='text-md p-1 font-medium tracking-wide leading-7'>{note.description}</h3>
-        </div>
-        ))
-    ) : (
+        {notes.length > 0 ? (
+            notes.map((note)=> (
+            <div key={note._id} className='h-auto w-94 bg-slate-500 text-white rounded-2xl p-2'>
+                <h1 className='text-2xl p-1 font-bold tracking-wider'>{note.title}</h1>
+                <h3 className='text-md p-1 font-medium tracking-wide leading-7'>{note.description}</h3>
+            </div>
+            ))
+        ) : (
+            <Addbtn/>
+        )
+    }   
         <Addbtn/>
-    )
-}   
-    <Addbtn/>
-    </section>
+        </section>
 
+        </div>
     </>
   )
 }
