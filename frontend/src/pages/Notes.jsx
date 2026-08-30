79ae9fa12b7383543a15d20057819e6ef36971f3
Akash Sharma <skysharma777@gmail.com>
2026-08-30 23:59:38 +0530

    UI update

--- frontend/src/pages/Notes.jsx
+++ frontend/src/pages/Notes.jsx
@@ -20,14 +20,14 @@ const Notes = () => {
     <div className='bg-[#22242a] mb-16'>
         
         <div className='min-h-screen relative'>
-        
-        <section className='flex flex-col flex-wrap justify-center items-center gap-6'>
+        <h1 className='text-white text-3xl font-extrabold absolute top-6 left-8'>Collections:</h1>
+        <section className='flex flex-col flex-wrap justify-center items-center gap-6 p-4'>
 
         {notes.length > 0 ? (
             notes.map((note)=> (
-            <div key={note._id} className='h-auto w-96 bg-slate-500 text-white rounded-2xl p-4 first:mt-8'>
-                <h1 className='text-2xl p-1 font-black tracking-wider'>{note.title}</h1>
-                <h3 className='text-md p-1 font-bold tracking-wide leading-7'>{note.description}</h3>
+            <div key={note._id} className='h-auto w-96 bg-slate-500 text-white rounded-2xl p-4 first:mt-20'>
+                <h1 className='text-2xl p-1 ml-2 font-black tracking-wider'>{note.title}</h1>
+                <h3 className='text-md p-1 ml-2 font-bold tracking-wide leading-7'>{note.description}</h3>
             </div>
             ))
         ) : (
