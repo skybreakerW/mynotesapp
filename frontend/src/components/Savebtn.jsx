24bc0ee89110a0b1495175a1cc535b0c9b64e313
Akash Sharma <skysharma777@gmail.com>
2026-08-30 23:04:35 +0530

    modified

--- frontend/src/components/Savebtn.jsx
+++ frontend/src/components/Savebtn.jsx
@@ -3,8 +3,8 @@ import { SavePlus } from 'lucide-react';
 
 const Savebtn = () => {
   return (
-    <button type='submit' className='bg-slate-600 flex flex-row items-center justify-center gap-4 p-4 rounded-4xl tracking-widest font-bold text-2xl cursor-pointer'>
-      <SavePlus size={40} color="#ffffff" />Save
+    <button type='submit' className='bg-transparent rounded-full w-max fixed bottom-25 right-12 cursor-pointer'>
+      <SavePlus size={30} color="#ffffff" />
     </button>
   )
 }
